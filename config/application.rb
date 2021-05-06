b9e7bddaebfa9d9cfa5c48ca587bcb05e5f9da70
noguchi <02125011a@gmail.com>
2021-05-06 09:43:18 +0900

    [update]edit dotenv-rails for config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,4 +19,8 @@ module Kinoko2
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
+
+  # 環境変数の設定
+  Dotenv::Railtie.load
+  HOSTNAME = ENV['HOSTNAME']
 end
