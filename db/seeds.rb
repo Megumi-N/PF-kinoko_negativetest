1c1d674ab84b7df5137495ae0d539c0a97632e49
noguchi <02125011a@gmail.com>
2021-05-24 15:19:20 +0900

    [update]add sample date

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,39 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-9.times do |n|
-  Result.create!(
-    name: "name#{n+1}",
-    feature: "feature#{n+1}",
-    contents: "contents#{n+1}"
-  )
+
+10.times do |n|
+  r = Result.create!(
+        name: "name#{n+1}",
+        level: "level#{n+1}",
+        feature: "feature#{n+1}",
+        description: "description#{n+1}",
+        contents: "contents#{n+1}",
+        image: "#{n+1}.png",
+      )
+  3.times do |m|
+    r.wise_sayings.create!(
+      title: "title#{m+1}",
+      description: "description#{m+1}",
+      person: "person#{m+1}",
+    )
+  end
 end
+
+# WiseSaying.create!(
+#   title: "title#{n+1}",
+#   description: "description#{n+1}",
+#   person: "person#{n+1}"
+# )
+# 9.times do |n|
+#   Result.create!(
+#     name: "name#{n+1}",
+#     level: "level#{n+1}",
+#     feature: "feature#{n+1}",
+#     description: "description#{n+1}",
+#     contents: "contents#{n+1}",
+#     image: "#{n+1}.png",
+#     wise_sayings_id: "#{n+1}"
+#   )
+# end
+
