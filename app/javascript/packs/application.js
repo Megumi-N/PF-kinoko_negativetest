1516c9ee62da73f85fd1a6fc9a37b412fc615367
noguchi <02125011a@gmail.com>
2021-06-01 23:53:55 +0900

    [fix]enable both bootstrap and jquery #13

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,8 +10,8 @@ import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
-import "bootstrap";
 import "jquery";
+import "bootstrap";
 import "../stylesheets/application";
 
 Rails.start();
