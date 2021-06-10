38e7658a283a7727c53994fcaa09915723b11c76
noguchi <02125011a@gmail.com>
2021-06-10 22:18:26 +0900

    [update]error handling for Twitter::Error::NotFound and Twitter::Error::Unauthorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,46 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base  
+  before_action :user_params
+
+  # if Rails.env.production?
+  #   rescue_from ActionController::RoutingError, with: :render_404 
+  #   rescue_from Exception, with: :render_500
+  # end
+
+  rescue_from Twitter::Error::NotFound, with: :not_found
+  rescue_from Twitter::Error::Unauthorized, with: :unauthorized
+
+  
+  # def render_404
+  #   render template: 'errors/error_404', status: 404
+  # end
+
+  # def render_500
+  #   render template: 'errors/error_500', status: 500
+  # end
+
+  def unauthorized
+    flash[:danger] = "非公開アカウントです。公開アカウントを入力してください。"
+    redirect_back(fallback_location: root_path)
+  end
+
+  def not_found
+    flash[:danger] = "有効なアカウントを入力してください。"
+    redirect_back(fallback_location: root_path)
+  end  
+
+  
+  private
+
+  def user_params
+    @user = params[:user]
+  end
+  
+  # def twitter_client
+  #   @client ||= Twitter::REST::Client.new do |config|
+  #     config.consumer_key           = Rails.application.credentials.twitter[:api_key]
+  #     config.consumer_secret        = Rails.application.credentials.twitter[:api_secret_key]
+  #     config.access_token           = Rails.application.credentials.twitter[:access_token]
+  #     config.access_token_secret    = Rails.application.credentials.twitter[:access_token_secret]
+  #   end
+  # end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'top#index'
   get 'results/index'
+
+  match '*path' => 'application#render_404', via: :all
   # post 'results/index', to: 'results#twitter_analysis'
   # post 'results/twitter_analysis', to: 'results#twitter_analysis'
   # get '/tweet', to: 'twitter_analyses#twitter_analysis'
