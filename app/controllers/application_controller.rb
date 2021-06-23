412690aeb7a5666ce296834a7fff087553fc94fb
noguchi <02125011a@gmail.com>
2021-06-24 00:59:07 +0900

    [update]add 429 error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,7 @@ class ApplicationController < ActionController::Base
 
   rescue_from Twitter::Error::NotFound, with: :not_found
   rescue_from Twitter::Error::Unauthorized, with: :unauthorized
-
-  def render_404
-    render template: 'errors/error_404', status: 404
-  end
+  rescue_from Twitter::Error::TooManyRequests, with: :too_many_requests
 
   def render_500(e)
     logger.error "error_500: #{e.message}"
@@ -16,13 +13,21 @@ class ApplicationController < ActionController::Base
     render template: 'errors/error_500', status: 500
   end
 
-  def unauthorized
-    flash[:danger] = "非公開アカウントだよ。公開アカウントを入力してね。"
-    redirect_back(fallback_location: root_path)
+  def render_404
+    render template: 'errors/error_404', status: 404
   end
 
   def not_found
     flash[:danger] = "有効なアカウントを入力してね。"
     redirect_back(fallback_location: root_path)
+  end
+
+  def unauthorized
+    flash[:danger] = "非公開アカウントだよ。公開アカウントを入力してね。"
+    redirect_back(fallback_location: root_path)
+  end
+
+  def too_many_requests
+    render template: 'errors/error_429', status: 429
   end
 end
