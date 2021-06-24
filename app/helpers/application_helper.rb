28f5fe3fef8ff10c8f43e927fe3ff9a91ece049f
noguchi <02125011a@gmail.com>
2021-06-24 19:05:40 +0900

    [update]change helper method full_url, from staging to production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,8 +13,7 @@ module ApplicationHelper
 
   # 参照urlをdevとprodで分けるメソッド
   def full_url(path)
-    # domain = if Rails.env.development? ? 'http://0.0.0.0:3000' : 'https://kinokoshindan.herokuapp.com/'
-    domain =  Rails.env.development? ? 'http://0.0.0.0:3000' : 'https://kinokoshindan-staging.herokuapp.com'
+    domain =  Rails.env.development? ? 'http://0.0.0.0:3000' : 'https://kinokoshindan.herokuapp.com'
     "#{domain}#{path}"
   end
 
