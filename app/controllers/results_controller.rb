68b5b8702a981b06bdd29ceafa17235c36f85070
noguchi <02125011a@gmail.com>
2021-06-27 08:29:02 +0900

    [fix]modified ogp url temporary

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,9 +1,11 @@
 class ResultsController < ApplicationController
-  def index
-    if request.referrer == root_url
-      # negativeの平均割合から分岐
-      twitter_analysis
+  # before_action :add_url_parameter, only: :index
 
+  def index
+    # negativeの平均割合から分岐
+      # @ave = 0.60
+
+      twitter_analysis
       case @ave
       when 0.90...1.00
         kinoko = 1
@@ -29,50 +31,43 @@ class ResultsController < ApplicationController
       @result = Result.find(kinoko)
       @wise_sayings = @result.wise_sayings.sample(3)
       @share = twitter_share
-    else
-      redirect_to root_url
-    end
+
   end
 
   # twitter分析メソッド
   def twitter_analysis
-
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = Rails.application.credentials.twitter[:api_key]
       config.consumer_secret     = Rails.application.credentials.twitter[:api_secret_key]
       config.access_token        = Rails.application.credentials.twitter[:access_token]
       config.access_token_secret = Rails.application.credentials.twitter[:access_token_secret]
     end
-
     @user = user_params[:user]
     @account = client.user(@user) # アカウントが存在するかどうか確認、一致しなかった場合Twitter::Error::NotFoundが発生
-    
-    if Rails.env.production?
-      @tweets = []
-      # exclude_replies: true => 返信を除去, include_rts: false => retweetを除去
-      client.user_timeline(@user, exclude_replies: true, include_rts: false).take(10).each do |tw|
-        # ハッシュ、url、空欄、改行、ファイルを削除
-        @tweets << tw.text.gsub(/#.*$/, "").gsub(/http.*\s/, "").gsub(/[ 　]+/,"").gsub(/\n/,"").gsub(/http.*\z/,"")
-      end
 
-      twitter_params = {
-        text_list: @tweets,
-        language_code: "ja"
-      }
-
-      comprehend = Aws::Comprehend::Client.new(region: 'us-east-1')
-      nega = comprehend.batch_detect_sentiment(twitter_params).result_list
-
-      i = 0
-      nega2=0
-      while i < nega.length do
-        nega2 += nega[i].sentiment_score.negative
-        i+=1
-      end
-      @ave = (nega2/nega.length).truncate(2)
-    else
-      @ave= 0.00
+    @tweets = []
+    # exclude_replies: true => 返信を除去, include_rts: false => retweetを除去
+    client.user_timeline(@user, exclude_replies: true, include_rts: false).take(10).each do |tw|
+      # ハッシュ、url、空欄、改行、ファイルを削除
+      @tweets << tw.text.gsub(/#.*$/, "").gsub(/http.*\s/, "").gsub(/[ 　]+/,"").gsub(/\n/,"").gsub(/http.*\z/,"")
     end
+
+    twitter_params = {
+      text_list: @tweets,
+      language_code: "ja"
+    }
+
+    comprehend = Aws::Comprehend::Client.new(region: 'us-east-1')
+    nega = comprehend.batch_detect_sentiment(twitter_params).result_list
+
+    i = 0
+    nega2=0
+    while i < nega.length do
+      nega2 += nega[i].sentiment_score.negative
+      i+=1
+    end
+
+    @ave = (nega2/nega.length).truncate(2)
   end
 
   def twitter_share
@@ -86,16 +81,18 @@ class ResultsController < ApplicationController
     end
 
     base = "https://twitter.com/intent/tweet?text="
-    tweet_contents = "#{@account.name}は#{@result.name}タイプ%0a🍄特性:#{@result.feature}%0a🍄ネガティブレベル:#{@result.level}%0a"+ text
-    hashtags = "%0a%20%23きのこネガティブ診断%20%20%23きのこ%0a"
-    link = "&url=#{request.url}"
-    # link = "&url=#{root_url}"
-    shareURL = base + tweet_contents + hashtags + link
+    tweet_contents = "#{@account.name}は#{@result.name}タイプ%0a🍄特性:#{@result.feature}%0a🍄ネガティブレベル:#{@result.level}%0a"+ text + "%0a%23きのこ%20%0a%23きのこネガティブ診断%20%0a"
+    # shareURL = base + tweet_contents + @url_parameter
+    shareURL = base + tweet_contents + root_url
   end
 
   private
 
   def user_params
     params.permit(:user)
+  end
+
+  def add_url_parameter
+    @url_parameter = url_for(controller: 'results', action: :index, id: SecureRandom.hex(10))
   end
 end
