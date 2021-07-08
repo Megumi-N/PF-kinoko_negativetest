b9ed8df04e2ad7f7291003ca261aa4653458d9e5
noguchi <02125011a@gmail.com>
2021-07-08 11:48:38 +0900

    [update]replace referrer_root_url? from results_controller to application, and modified regular Expression

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,55 +1,49 @@
 class ResultsController < ApplicationController
+  before_action :client_pass
+  before_action :referrer_root_url?, only: %i(index)
+
   # before_action :add_url_parameter, only: :index
 
   def index
     # negativeの平均割合から分岐
-      # @ave = 0.60
-      # @user = user_params[:user]
-      request.referrer!=root_url ? false : twitter_analysis
-      case @ave
-      when 0.90...1.00
-        kinoko = 1
-      when 0.80...0.90
-        kinoko = 2
-      when 0.70...0.80
-        kinoko = 3
-      when 0.60...0.70
-        kinoko = 4
-      when 0.50...0.60
-        kinoko = 5
-      when 0.40...0.50
-        kinoko = 6
-      when 0.30...0.40
-        kinoko = 7
-      when 0.20...0.30
-        kinoko = 8
-      when 0.10...0.20
-        kinoko = 9
-      else
-        kinoko = 10
-      end
-      @result = Result.find(kinoko)
-      @wise_sayings = @result.wise_sayings.sample(3)
-      @share = twitter_share
-
+    twitter_analysis
+    case @ave
+    when 0.90...1.00
+      kinoko = 1
+    when 0.80...0.90
+      kinoko = 2
+    when 0.70...0.80
+      kinoko = 3
+    when 0.60...0.70
+      kinoko = 4
+    when 0.50...0.60
+      kinoko = 5
+    when 0.40...0.50
+      kinoko = 6
+    when 0.30...0.40
+      kinoko = 7
+    when 0.20...0.30
+      kinoko = 8
+    when 0.10...0.20
+      kinoko = 9
+    else
+      kinoko = 10
+    end
+    @result = Result.find(kinoko)
+    @wise_sayings = @result.wise_sayings.sample(3)
+    @share = twitter_share
   end
 
   # twitter分析メソッド
   def twitter_analysis
-    client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = Rails.application.credentials.twitter[:api_key]
-      config.consumer_secret     = Rails.application.credentials.twitter[:api_secret_key]
-      config.access_token        = Rails.application.credentials.twitter[:access_token]
-      config.access_token_secret = Rails.application.credentials.twitter[:access_token_secret]
-    end
     @user = user_params[:user]
-    @account = client.user(@user) # アカウントが存在するかどうか確認、一致しなかった場合Twitter::Error::NotFoundが発生
-
+    @account = @client.user(@user) # アカウントが存在するかどうか確認、一致しなかった場合Twitter::Error::NotFoundが発生
     @tweets = []
+
     # exclude_replies: true => 返信を除去, include_rts: false => retweetを除去
-    client.user_timeline(@user, exclude_replies: true, include_rts: false).take(10).each do |tw|
-      # ハッシュ、url、空欄、改行、ファイルを削除
-      @tweets << tw.text.gsub(/#.*$/, "").gsub(/http.*\s/, "").gsub(/[ 　]+/,"").gsub(/\n/,"").gsub(/http.*\z/,"")
+    @client.user_timeline(@user, exclude_replies: true, include_rts: false).take(5).each do |tw|
+      # ハッシュ、url、空欄、改行、ファイルを削除。.gsub(/https?:\/\/+t.co\/.*$/,"")
+      @tweets << tw.text.gsub(/#.*$/, "").gsub(/http.*\s/, "").gsub(/[ 　]+/,"").gsub(/\n/,"").gsub(/http.*\/\/t.co\/.*$/, "")
     end
 
     twitter_params = {
@@ -57,7 +51,10 @@ class ResultsController < ApplicationController
       language_code: "ja"
     }
 
-    comprehend = Aws::Comprehend::Client.new(region: 'us-east-1')
+    comprehend = Aws::Comprehend::Client.new(
+      region: 'us-east-1',
+    )
+
     nega = comprehend.batch_detect_sentiment(twitter_params).result_list
 
     i = 0
@@ -81,9 +78,8 @@ class ResultsController < ApplicationController
     end
 
     base = "https://twitter.com/intent/tweet?text="
-    # tweet_contents = "#{@account.name}は#{@result.name}タイプ%0a🍄特性:#{@result.feature}%0a🍄ネガティブレベル:#{@result.level}%0a"+ text + "%0a%23きのこ%20%0a%23きのこネガティブ診断%20%0a"
-    tweet_contents = "#{@result.name}タイプ%0a🍄特性:#{@result.feature}%0a🍄ネガティブレベル:#{@result.level}%0a"+ text + "%0a%23きのこ%20%0a%23きのこネガティブ診断%20%0a"
-    add_url_parameter
+    tweet_contents = "#{@account.name}さんは...%0a#{@result.name}タイプ%0a🍄特性:#{@result.feature}%0a🍄ネガティブレベル:#{@result.level}%0a"+ text + "%0a%23きのこ%20%0a%23きのこネガティブ診断%20%0a"
+    # add_url_parameter
     # shareURL = base + tweet_contents + @url_parameter
     shareURL = base + tweet_contents + root_url
   end
