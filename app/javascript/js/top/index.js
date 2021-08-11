15bf3a2efed74654caab627e2e9fce6a8dc3fe89
noguchi <02125011a@gmail.com>
2021-08-11 19:56:38 +0900

    [update]add kinoko balloons fadeout function

--- app/javascript/js/top/index.js
+++ app/javascript/js/top/index.js
@@ -5,8 +5,8 @@ window.onload = function () {
   const $submitButton = document.forms[0].submit;
 
   $checkBox.addEventListener("change", () => {
-    let checkBoxCheck = $checkBox.checked
-    
+    let checkBoxCheck = $checkBox.checked;
+
     if (checkBoxCheck == false) {
       $submitButton.disabled = "false";
       $submitButton.classList.value = "button_disabled";
@@ -16,21 +16,53 @@ window.onload = function () {
       $submitButton.removeAttribute("disabled");
       $submitButton.classList.value = "button";
       // $checkBox.checked = "checked";
-      $checkBox.setAttribute("checked", "checked")
+      $checkBox.setAttribute("checked", "checked");
     }
   });
-    // let checkBoxVal = $checkBox.value;
-    // if (checkBoxVal == 1) {
-    //   $submitButton.removeAttribute("disabled");
-    //   $submitButton.classList.value="button"
-    //   $checkBox.value = 0;
-    // } else {
-    //   $submitButton.disabled = "false";
-    //   $submitButton.classList.value="button_disabled"
-    //   $checkBox.value = 1;
-    // }
+  // let checkBoxVal = $checkBox.value;
+  // if (checkBoxVal == 1) {
+  //   $submitButton.removeAttribute("disabled");
+  //   $submitButton.classList.value="button"
+  //   $checkBox.value = 0;
+  // } else {
+  //   $submitButton.disabled = "false";
+  //   $submitButton.classList.value="button_disabled"
+  //   $checkBox.value = 1;
+  // }
 
   // イベントリスナの処理の外にfalseを定義してcheckedを初期のfalseに戻す
   $checkBox.checked = false;
 };
 
+const arr = document.getElementsByClassName("kinoko");
+
+for (let i = 0; i < 5; i++) {
+  arr[i].addEventListener("click", () => {
+    fadeOut(arr[i], 300);
+    setTimeout(function () {
+      arr[i].style.display = "";
+    }, 5000);
+  });
+}
+
+function fadeOut(node, duration) {
+  node.style.opacity = 1;
+
+  const start = performance.now();
+
+  requestAnimationFrame(function tick(timestamp) {
+    // イージング計算式（linear）
+    const easing = (timestamp - start) / duration;
+
+    // opacityが0より小さくならないように
+    node.style.opacity = Math.max(1 - easing, 0);
+
+    // イージング計算式の値が1より小さいとき
+    if (easing < 1) {
+      requestAnimationFrame(tick);
+    } else {
+      node.style.opacity = "";
+      node.style.display = "none";
+    }
+  });
+}
--- app/javascript/packs/top/index.js
+++ app/javascript/packs/top/index.js
@@ -1,2 +1,1 @@
 require("../../js/top/index");
-
