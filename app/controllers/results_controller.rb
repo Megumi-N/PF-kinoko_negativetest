216365f21192274d0ad53762af6dfb2fcb4fc198
noguchi <02125011a@gmail.com>
2021-09-21 02:57:22 +0900

    [Fix]set negative_degree in model/result.rb

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -12,43 +12,33 @@ class ResultsController < ApplicationController
   # twitter分析メソッド
   def twitter_analysis
     @user = user_params[:user]
-
     @account = @client.user(@user) # アカウントが存在するかどうか確認、一致しなかった場合Twitter::Error::NotFoundが発生
+    # @tweets = []
+    # @client.user_timeline(@user, exclude_replies: true, include_rts: false).take(5).each do |tw|
+    #   tweet = tw.text.gsub(/#.*$|[ 　]+|\n|http.*:\/\/t.co\/\w*$/,"") # ハッシュ、空欄、改行、twitterの省略url
+    #   @tweets << tweet if tweet.present?
+    # end
 
-    @tweets = []
-    @client.user_timeline(@user, exclude_replies: true, include_rts: false).take(5).each do |tw|
-      tweet = tw.text.gsub(/#.*$|[ 　]+|\n|http.*:\/\/t.co\/\w*$/,"") # ハッシュ、空欄、改行、twitterの省略url
-      @tweets << tweet if tweet.present?
-    end
+    # twitter_params = {
+    #   text_list: @tweets,
+    #   language_code: "ja"
+    # }
+    # comprehend = Aws::Comprehend::Client.new(
+    #   region: 'us-east-1',
+    # )
 
-    twitter_params = {
-      text_list: @tweets,
-      language_code: "ja"
-    }
-    comprehend = Aws::Comprehend::Client.new(
-      region: 'us-east-1',
-    )
+    # comprehend_list = comprehend.batch_detect_sentiment(twitter_params).result_list
+    # negative_point = 0
+    # comprehend_list.each_index {|i| negative_point += comprehend_list[i].sentiment_score.negative }
 
-    comprehend_list = comprehend.batch_detect_sentiment(twitter_params).result_list
-    negative_point = 0
-    comprehend_list.each_index {|i| negative_point += comprehend_list[i].sentiment_score.negative }
-
-    # @negative_level=(0.21.ceil(1))*10
+    @negative_level=(0.21.ceil(1))*10
     # (negative_point/comprehend_list.length)でnegative_pointの平均値を算出し、
     # transcaleで少数第二位までの値を取得、ceilで切り上げを行ってlevelに合致する値を算出する
-    @@negative_level = (negative_point/comprehend_list.length).truncate(2).ceil(1)*10
+    # @negative_level = (negative_point/comprehend_list.length).truncate(2).ceil(1)*10
   end
 
   def twitter_share
-    case @result.level
-    when 1...4
-      text = "🍄ネガティブ度:低"
-    when 4...8
-      text = "🍄ネガティブ度:中"
-    else
-      text = "🍄ネガティブ度:高"
-    end
-
+    text = @result.negative_degree
     base = "https://twitter.com/intent/tweet?text="
     # text部分はエンコード化することで400エラーを防ぐことができる
     tweet_contents = "#{URI.encode_www_form_component(@account.name)}さんは...%0a#{@result.name}タイプ%0a🍄特性:#{@result.feature}%0a🍄ネガティブレベル:#{@result.level}%0a"+ text + "%0a%23きのこ%20%0a%23きのこネガティブ診断%20%0a"
