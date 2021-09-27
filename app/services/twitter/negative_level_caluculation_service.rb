956377a53cabc6c6f4530739a2a339570457f2b5
noguchi <02125011a@gmail.com>
2021-09-28 03:24:54 +0900

    [fix]refactor service class, and fix bug

--- app/services/twitter/negative_level_caluculation_service.rb
+++ app/services/twitter/negative_level_caluculation_service.rb
@@ -9,29 +9,31 @@ class Twitter::NegativeLevelCaluculationService
   end
 
   def call
-    @account = client.user(@user) # アカウントが存在するかどうか確認、一致しなかった場合Twitter::Error::NotFoundが発生
-
-    @tweets = []
-    @client.user_timeline(@user, exclude_replies: true, include_rts: false).take(5).each do |tw|
-      tweet = tw.text.gsub(/#.*$|[ 　]+|\n|http.*:\/\/t.co\/\w*$/,"") # ハッシュ、空欄、改行、twitterの省略url
-      @tweets << tweet if tweet.present?
-    end
-
-    twitter_params = {
-      text_list: @tweets,
-      language_code: "ja"
-    }
-    comprehend = Aws::Comprehend::Client.new(
-      region: 'us-east-1',
-    )
+    comprehend = Aws::Comprehend::Client.new( region: 'us-east-1')
 
     comprehend_list = comprehend.batch_detect_sentiment(twitter_params).result_list
     negative_point = 0
-    comprehend_list.each_index {|i| negative_point += comprehend_list[i].sentiment_score.negative }
-    @average = (negative_point/comprehend_list.length).truncate(2).ceil(1)*10
+    comprehend_list_length = comprehend_list.each_index {|i| negative_point += comprehend_list[i].sentiment_score.negative }
+    (negative_point/comprehend_list_length.length).truncate(2).ceil(1)*10
   end
 
   private
 
   attr_reader :screen_name, :client # インスタンス変数の@がなしで呼び出せるようになる
+
+  def tweets
+    tweets = []
+    client.user_timeline(screen_name, exclude_replies: true, include_rts: false).take(5).each do |tw|
+      tweet = tw.text.gsub(/#.*$|[ 　]+|\n|http.*:\/\/t.co\/\w*$/,"") # ハッシュ、空欄、改行、twitterの省略url
+      tweets << tweet if tweet.present?
+    end
+    tweets
+  end
+
+  def twitter_params
+    {
+      text_list: tweets, # tweetsメソッドが呼び出される
+      language_code: "ja"
+    }
+  end
 end
